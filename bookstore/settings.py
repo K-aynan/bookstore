bae5d68fb8c97a995bf9747055ab413f41de5216
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-08-24 09:38:32 -0300

    i'm done

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -123,3 +123,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framwork.authentication.SessionAuthentication',
+    ],
+}
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
